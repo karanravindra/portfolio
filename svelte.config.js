85acaaca215c5881b131f8526d28cbf6305702e1
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-03-19 15:23:00 -0400

    Vite Alias

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		// Aliases for vite
+		alias: {
+			$components: '/src/lib/components',
+			$assets: '/src/lib/assets',
+			$utils: '/src/lib/utils'
+		}
 	}
 };
 
