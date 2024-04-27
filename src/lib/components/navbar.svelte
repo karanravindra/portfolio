a03c65336c013a5f938cafa46cb139b23fbf8042
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-04-26 22:40:35 -0400

    fix navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -4,22 +4,127 @@
 	import logo_light from '$assets/logo-light.svg';
 	import logo_dark from '$assets/logo-dark.svg';
 	$: activeUrl = $page.url.pathname;
+
+	let displayOpen = false;
 </script>
 
-<Navbar>
-	<NavBrand href="/">
-		<img src={logo_light} class="me-3 h-6 sm:h-9 dark:hidden" alt="Portfolio Logo" />
-		<img src={logo_dark} class="me-3 hidden h-6 sm:h-9 dark:block" alt="Portfolio Logo" />
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-			>Karan Ravindra</span
+<nav class="border-gray-200 bg-white dark:bg-gray-900">
+	<div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+		<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<img src={logo_light} class="me-3 h-6 sm:h-9 dark:hidden" alt="Portfolio Logo" />
+			<img src={logo_dark} class="me-3 hidden h-6 sm:h-9 dark:block" alt="Portfolio Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>Karan Ravindra</span
+			>
+		</a>
+		<button
+			data-collapse-toggle="navbar-default"
+			type="button"
+			class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+			aria-controls="navbar-default"
+			aria-expanded="false"
+			on:click={() => (displayOpen = !displayOpen)}
 		>
-	</NavBrand>
-	<NavHamburger />
-	<NavUl {activeUrl}>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="#about">About</NavLi>
-		<!-- <NavLi href="/blog">Blog</NavLi>
-		<NavLi href="#projects">Projects</NavLi> -->
-		<NavLi href="#contact">Contact</NavLi>
-	</NavUl>
-</Navbar>
+			<span class="sr-only">Open main menu</span>
+			<svg
+				class="h-5 w-5"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 17 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M1 1h15M1 7h15M1 13h15"
+				/>
+			</svg>
+		</button>
+		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+			<ul
+				class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900"
+			>
+				<li>
+					<a
+						href="/"
+						class="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white md:dark:text-blue-500"
+						aria-current="page">Home</a
+					>
+				</li>
+				<li>
+					<a
+						href="#about"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>About</a
+					>
+				</li>
+				<!-- <li>
+					<a
+						href="#blog"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>Blog</a
+					>
+				</li>
+				<li>
+					<a
+						href="#projects"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>Projects</a
+					>
+				</li> -->
+				<li>
+					<a
+						href="#contact"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>Contact</a
+					>
+				</li>
+			</ul>
+		</div>
+		{#if displayOpen}
+			<div class="w-full md:block md:w-auto" id="navbar-default">
+				<ul
+					class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900"
+				>
+					<li>
+						<a
+							href="/"
+							class="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:text-white md:dark:text-blue-500"
+							aria-current="page">Home</a
+						>
+					</li>
+					<li>
+						<a
+							href="#about"
+							class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+							>About</a
+						>
+					</li>
+					<!-- <li>
+					<a
+						href="#blog"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>Blog</a
+					>
+				</li>
+				<li>
+					<a
+						href="#projects"
+						class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+						>Projects</a
+					>
+				</li> -->
+					<li>
+						<a
+							href="#contact"
+							class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+							>Contact</a
+						>
+					</li>
+				</ul>
+			</div>
+		{/if}
+	</div>
+</nav>
